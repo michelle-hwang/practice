c30d52bf4c0fc3b050196921df0c3441d4cd0c18
michelle-hwang <mxhwang@gmail.com>
2016-11-01 14:43:34 -0400

    modified:   breadth-first-traversal.cpp

--- graphs/breadth-first-traversal.cpp
+++ graphs/breadth-first-traversal.cpp
@@ -1,16 +1,45 @@
 #include<iostream>
 #include <list>
 
-// Write a function to print the BFS for a graph given source s
-
 class Graph {
-	int V; // num vertices
+	int N; // num nodes
 	list<int> *adj; // pointer to array containing adjacency lists
 
 public:
-	Graph(int V); // constructor
-	void addEdge(int v, int w);
-	void BFS(int s); 
+	Graph(int N); // constructor
+	void add(int n, int x); // Add value x to node n
+	void bfs(int s); 
 };
 
+Graph::Graph(int N) {
+	this->N = N;
+	adj = new list<int>[N];
+}
+
+void Graph::add(int n, int x) {
+	adj[n].push_back(x);
+}
+
+void bfs(int s) {
+
+}
+
+int main() {
+
+	// Test graph 1
+	Graph g(4);
+	g.add(0, 1);
+	g.add(0, 2);
+	g.add(1, 2);
+	g.add(2, 0);
+	g.add(2, 3);
+	g.add(3, 3);
+
+	g.bfs;
+
+	return;
+}
+
+
+main();
 
