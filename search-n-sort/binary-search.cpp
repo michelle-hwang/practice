e7b0fb5f5cd46ca701f6565cdacdc4bbe1d5a086
michelle-hwang <mxhwang@gmail.com>
2016-11-01 15:07:25 -0400

    new file:   binary-search.cpp

---
+++ search-n-sort/binary-search.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+// Search sorted array to find index location of a given element
+
+int binarySearch(int a[], int x) {
+	int n = sizeof(a);
+	int i = 0;
+
+	while(i < n) {
+		int mid = i + (n - 1) / 2; 
+
+		if(a[mid] < x)
+			i = mid + 1;
+		else
+			n = mid - 1	
+	}
+	// Element not present
+	return -1;
+}
+
+int binarySearchRecursive(int a[], int x, int i, int n) {
+	if (n >= i) {
+		int m = i + (n - 1) / 2;
+
+		if (a[mid] == x) 
+			return m;
+		else if (a[m] > x)
+			return binarySearchRecursive(a, x, i, m - 1);
+		else
+			return binarySearchRecrusive(a, x, m + 1, n);
+	}
+
+	// Element not present
+	return -1
+}
+
+int main() {
+}
+
+main()
